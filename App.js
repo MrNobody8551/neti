6654382765c03242b33cde4f0ffc2e372fe4418e
MrNobody8551 <116880381+MrNobody8551@users.noreply.github.com>
2023-11-16 14:17:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import { LandingPage } from './components/LandingPage';
+import 'src/components/LandingPage.css';
+
+
+
+
+
+
+function App() {
+  return (
+
+    <>
+    
+    <div className="App">
+      <header className="App-header">
+
+          <LandingPage /> 
+            
+          
+
+      </header>
+    </div>
+    </>
+  );
+  
+  }
+
+export default App;
